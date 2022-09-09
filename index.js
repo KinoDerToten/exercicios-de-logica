81d87dc3d2043691726844cb291fd69ec4b29f87
KinoDerToten <51245583+KinoDerToten@users.noreply.github.com>
2022-09-09 19:01:06 -0400

    tabuada de 5

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const conjuntoDeNumeros = 500;
+/*const conjuntoDeNumeros = 500;
 let somaImpares = 0;
 
 for (let i = 1; i <= 500; i++){
@@ -7,4 +7,17 @@ for (let i = 1; i <= 500; i++){
   }
 }
 
-console.log(somaImpares)
+console.log(somaImpares)*/
+
+/*for(let min = 9; min >= 0; min--){
+  for(let sec = 59; sec >= 0; sec--){
+    console.log(min,':', sec);
+  }
+}*/
+
+
+/*let numberFive = 5;
+for(let i = 0; i <= 10; i++){
+  console.log(`${numberFive} X ${i} = ${numberFive*i}`);
+}*/
+
